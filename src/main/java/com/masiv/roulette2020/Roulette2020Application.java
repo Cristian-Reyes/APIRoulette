18aa4d621013f9a42ae00d90ce90f2c163a7e3cb
Cristian_Reyes <ccristianreyesl@gmail.com>
2020-12-12 11:34:11 -0500

    Se crea el proyecto roulette2020 usando Spring Initializr

---
+++ src/main/java/com/masiv/roulette2020/Roulette2020Application.java
@@ -0,0 +1,13 @@
+package com.masiv.roulette2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roulette2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roulette2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/masiv/roulette2020/Roulette2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masiv.roulette2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roulette2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
